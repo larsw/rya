e77d6fc809213b85d2f0134582926e1055297083
Lars Wilhelmsen <lars@sral.org>
2020-10-22 09:31:17 +0000

    wip

--- web/web2.rya/frontend/src/App.tsx
+++ web/web2.rya/frontend/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Yasgui from "@triply/yasgui";
+import "@triply/yasgui/build/yasgui.min.css";
+
+function YG() {
+  useEffect(() => {
+    const yasgui = new Yasgui(document.getElementById("yasgui")!, {});
+  });
+
+  return <div id="yasgui" />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YG />
     </div>
   );
 }
